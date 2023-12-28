fecfb95aedc27956b093ee11f182a37229631bf8
Deepali Kasture <kasture.d@northeastern.edu>
2023-12-28 03:23:39 -0500

    Learning React 01

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+//react element
+const heading = React.createElement("h1",{id:"heading"},"Hello World in React!");
+
+//react element using jsx
+const JsxHeading = ()=>{ return <h1 id="heading" className="header">Hello World in React!</h1>}
+
+//React funcitonal component syntax examples
+const Fn = ()=>{
+    return <h1 id="heading" className="header">this is a functional component</h1>;
+}
+
+const Fn1 = ()=>(
+    <div className="container">
+        <JsxHeading />
+        <h1 id="heading" className="header">this is a functional component</h1>
+    </div>
+)
+//<fn2/> this is a react element
+const Fn2 = ()=> true;
+
+//React Dom will create a root for the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Fn1/>);
+
+//Example to create nested html elements using react
 const parent = React.createElement("div",{id:"parent"},
 [
     React.createElement("h1",{id:"child"},"I am h1 tag"),
-    React.createElement("h2",{},"I am h2 tag")
+    React.createElement("h2",{},"I am h2 tag here")
 
 ]
 )
-
-
-const heading = React.createElement("h1",{id:"heading"},"Hello World in React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
---
+++ README.md
@@ -0,0 +1,6 @@
+# Namaste React
+Topics covered:
+1 .Parcel and its features
+2 .JSX
+3. Creating React Elements
+4. Functional Components
